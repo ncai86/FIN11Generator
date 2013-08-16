3354f1d709373c21c4fbcf6dcd046c8e6eee78ff
Nicholas <nicholascai86@gmail.com>
2013-08-16 14:25:13 +0800

    adding scope

--- app/models/configuration_field.rb
+++ app/models/configuration_field.rb
@@ -2,7 +2,7 @@ class ConfigurationField < ActiveRecord::Base
 	belongs_to :configuration_name
   	attr_accessible :name, :default_value, :state
 
-  	validates :name, presence: true
 
   	scope :display, where(state: ["Mandatory", "Optional"])
+  	scope :disabled_fields, where(state: "Disabled")
 end
