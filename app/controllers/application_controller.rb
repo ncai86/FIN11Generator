a08c91dd768373161413f5bff5b0a42689a99518
Nicholas <nicholascai86@gmail.com>
2013-08-21 15:11:47 +0800

    added ip_identifer helper method to application controller for cache key usage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  helper_method :ip_identifer
+
+  def ip_identifer
+  	if request.env["HTTP_X_FORWARDED_FOR"]
+  		 ip =  request.env["HTTP_X_FORWARDED_FOR"].split(",").first
+  	elsif request.env["REMOTE_ADDR"]
+  		 ip = request.env["REMOTE_ADDR"]
+  	end
+  	ip
+  end
+
 end
