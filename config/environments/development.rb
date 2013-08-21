7598b3bdc3ae97edb61b6819bd0027ea6eb2462c
Nicholas <nicholascai86@gmail.com>
2013-08-21 15:03:54 +0800

    dalli configurations for development/production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ FIN11Generator::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
@@ -36,5 +36,7 @@ FIN11Generator::Application.configure do
   config.assets.debug = true
 
   config.serve_static_assets = false
-  
+
+  # config.cache_store = :file_store, "tmp/cache/development"
+  config.cache_store = :dalli_store
 end
