00dd3c423062ac1fe3735f8e17355654e3256924
Nicholas <nicholascai86@gmail.com>
2013-08-21 15:04:42 +0800

    adding ordered list for form parameters

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -36,12 +36,49 @@ FIELDS = [
 	"Date of last update",
 	"Terminal Status"]
 
+ORDERED_FIELD = [
+	"Version",
+	"Merchant ID",
+	"Merchant Category Code",
+	"Point of sale name",
+	"Point of sale address",
+	"Point of sale city",
+	"Point of sale province",
+	"Point of sale post code",
+	"Point of sale country",
+	"POS Contact Name",
+	"Point of sale telephone",
+	"Point of sale fax number",
+	"Point of Sale email",
+	"Company's Legal Name",
+	"Company's address",
+	"Company's city",
+	"Company's province",
+	"Company's post code",
+	"Company's country",
+	"Contact Name",
+	"Company's telephone",
+	"Company's fax number",
+	"Company's Email",
+	"VAT Number",
+	"Bank ID",
+	"Terminal ID",
+	"Creation date",
+	"Installation date",
+	"Manufacturer/model of the POS",
+	"Bank Agency Code",
+	"Bank Account",
+	"Terminal Manager",
+	"Date of last update",
+	"Terminal Status"
+	]
 
 
 
-HEADER_LINE = ["Volume header",
-		  "Activation date",
-		  "Activation time"]
+
+	HEADER_LINE = ["Volume header",
+		"Activation date",
+		"Activation time"]
 
 
 
