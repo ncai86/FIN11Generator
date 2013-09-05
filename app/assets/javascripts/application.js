7479bb5436a02daeb1680076c75485d92321ae29
Nicholas <nicholascai86@gmail.com>
2013-09-05 17:06:00 +0800

    correct date and time function, prepending 0 when single digit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,15 +23,15 @@
 function getDate(){
 	var date = new Date();
 	var year = date.getFullYear().toString();
-	var month = ( '0' + (date.getMonth()+1) ).slice( -2 );
-	var day = date.getDate().toString();
+	var month = ('0' + (date.getMonth()+1)).slice(-2);
+	var day = ('0' + date.getDate().toString()).slice(-2);
 	return ( year + month + day);
 }
 
 function getTime(){
 	var date = new Date();
-	var hour = date.getHours().toString();
-	var min = date.getMinutes().toString();
-	var sec = date.getSeconds().toString();
+	var hour = ('0' + date.getHours().toString()).slice(-2);
+	var min = ('0' + date.getMinutes().toString()).slice(-2);
+	var sec = ('0' + date.getSeconds().toString()).slice(-2);
 	return ( hour + min + sec );
 }
