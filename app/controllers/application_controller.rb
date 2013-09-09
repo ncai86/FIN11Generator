3cf0ba08c6ab04f38a32ab928be7de51f8251e4b
Nicholas <nicholascai86@gmail.com>
2013-09-09 15:44:50 +0800

    Adding session store when user accesses application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :user_identifer
   helper_method :ip_identifer
 
   def ip_identifer
@@ -11,5 +12,12 @@ class ApplicationController < ActionController::Base
   	end
   	ip
   end
+
+  def user_identifer
+    logger.info "ITSTIGERTIME!!!!!!!!!!!!!!!"
+    session[:user] ||= Time.now.to_i
+    logger.info session[:user]
+  end
+
 
 end
