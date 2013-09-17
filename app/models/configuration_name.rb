0f69c3da4ab8679fc93862eb08b815d85f38a05a
Nicholas <nicholascai86@gmail.com>
2013-09-17 16:09:29 +0800

    configuration relation to base currency

--- app/models/configuration_name.rb
+++ app/models/configuration_name.rb
@@ -1,5 +1,6 @@
 class ConfigurationName < ActiveRecord::Base
 	has_many :configuration_fields, :dependent => :destroy
+	has_many :base_currencies, :dependent => :destroy
   	attr_accessible :name
 
   	validates :name, presence: true
